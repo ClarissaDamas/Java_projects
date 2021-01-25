bb8c6bc1f443c1a042ec859dad5dc431c36ffa47
clarissaD <physicscla@gmail.com>
2021-01-25 20:29:13 -0300

    new files

---
+++ witchName/src/name/Map.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package name;
+
+/**
+ *
+ * @author eeccm
+ */
+class Map<T0, T1> {
+    
+}
